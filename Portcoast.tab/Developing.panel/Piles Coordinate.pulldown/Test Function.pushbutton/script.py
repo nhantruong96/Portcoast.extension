9060f36c2feb3c6dfcb001173b06059cc22a60de
nhantruong96 <nhan96bk@gmail.com>
2020-10-12 09:40:13 +0700

    Update script.py

--- Portcoast.tab/Developing.panel/Piles Coordinate.pulldown/Test Function.pushbutton/script.py
+++ Portcoast.tab/Developing.panel/Piles Coordinate.pulldown/Test Function.pushbutton/script.py
@@ -3,14 +3,14 @@
 __title__ = "Get Current Time"
 
 import os
-import datetime
-from datetime import datetime
+import sys
+import clr
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
 
-now = datetime.now()
-print(str(now.strftime("%c"))
+uidoc = __revit__.ActiveUIDocument
+doc = uidoc.Document
+app = __revit__.Application
 
-
-
-
-# newDef = Definitions()
-# newDef.Create(defOpt)
+itemsSelected = uidoc.Selection
+idsSelected = itemsSelected.GetElementId()
